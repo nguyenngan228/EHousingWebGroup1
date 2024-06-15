7a56efc651f5ac262e732a72a9d8831d15736be7
nguyengan228 <thanhngan.220803@gmail.com>
2024-06-16 01:08:09 +0700

    Init project

---
+++ MyHouseWeb_Group1/src/component/Landlord/home.js
@@ -0,0 +1,148 @@
+import React, { useState, useEffect } from 'react';
+import { Carousel, Card, Spinner } from 'react-bootstrap';
+import APIs, { endpoints } from '../../config/APIs';
+
+
+const HomeLandlord=()=> {
+    const [rooms, setRooms] = useState(null);
+
+    const tenantPost = async () => {
+        console.info('================')
+        try {
+            let res = await APIs.get(endpoints['tenantPost']);
+            setRooms(res.data)
+            console.info(res)
+        } catch (ex) {
+            console.error(ex);
+        }
+    }
+    useEffect(() => {
+        tenantPost();
+    }, [])
+
+
+    return (
+        <>
+            <Carousel className="banner">
+                <Carousel.Item>
+                    <img
+                        className="d-block w-100"
+                        src="/banner-1.jpg"
+                        alt="First slide"
+                    />
+                    <Carousel.Caption>
+                        <h3 className="text-white">Kênh thông tin Phòng Trọ số 1 Việt Nam</h3>
+                        <p>Kênh thông tin Phòng Trọ số 1 Việt Nam - Website đăng tin cho thuê phòng trọ, nhà nguyên căn,
+                            căn hộ, ở ghép nhanh, hiệu quả với 100.000+ tin đăng và 2.500.000 lượt xem mỗi tháng.</p>
+                    </Carousel.Caption>
+                </Carousel.Item>
+                <Carousel.Item>
+                    <img
+                        className="d-block w-100"
+                        src="/banner-2.jpg"
+                        alt="Second slide"
+                    />
+
+                    <Carousel.Caption>
+                        <h3 className="text-white">Chi phí thấp, hiệu quả tối đa</h3>
+                        <p>Không phải tốn nhiều công sức và chi phí cho việc đăng tin cho thuê: từ việc phát tờ rơi, dán
+                            giấy, và đăng lên các website khác nhưng hiệu quả không cao.</p>
+                    </Carousel.Caption>
+                </Carousel.Item>
+                <Carousel.Item>
+                    <img
+                        className="d-block w-100"
+                        src="/banner-3.jpg"
+                        alt="Third slide"
+                    />
+
+                    <Carousel.Caption>
+                        <h3 className="text-white">Bạn đang có phòng trọ / căn hộ cho thuê?</h3>
+                        <p>Không phải lo tìm người cho thuê, phòng trống kéo dài</p>
+                    </Carousel.Caption>
+                </Carousel.Item>
+            </Carousel>
+            <section className="search-sec">
+                <div className="container">
+                    <form action="#" method="post" noValidate="novalidate">
+                        <div className="row">
+                            <div className="col-lg-12">
+                                <div className="row">
+                                    {/* <div className="col-lg-6 col-12 p-0">
+                                        <input type="text" className="form-control search-slt"
+                                               placeholder="Tìm kiếm"/>
+                                    </div> */}
+                                    <div className="col-lg-3 col-12 p-0">
+                                        <select className="form-control search-slt" id="province" onChange={() => {}}>
+                                            <option value="">Chọn tỉnh thành</option>
+                                            {/* 
+                                            cities.map((item, index) => (
+                                                <option key={index} value={`${item.ProvinceID},${item.ProvinceName}`}>
+                                                    {item.ProvinceName}
+                                                </option>
+                                            )) 
+                                            */}
+                                        </select>
+                                    </div>
+                                    <div className="col-lg-3 col-12 p-0">
+                                        <select className="form-control search-slt" id="province" onChange={() => {}}>
+                                            <option value="">Chọn quận huyện</option>
+                                            {/* 
+                                            districts.map((item, index) => (
+                                                <option key={index} value={`${item.DistrictID},${item.DistrictName}`}>
+                                                    {item.DistrictName}
+                                                </option>
+                                            )) 
+                                            */}
+                                        </select>
+                                    </div>
+                                    <div className="col-lg-3 col-12 p-0">
+                                        <select className="form-control search-slt" id="province" onChange={() => {}}>
+                                            <option value="">Chọn quận huyện</option>
+                                            {/* 
+                                            wards.map((item, index) => (
+                                                <option key={index} value={`${item.WardCode},${item.WardName}`}>
+                                                    {item.WardName}
+                                                </option>
+                                            )) 
+                                            */}
+                                        </select>
+                                    </div>
+                                    <div className="col-lg-3 col-12 p-0">
+                                        <button onClick={() => {}} type="button" className="btn bg-gradient btn-danger wrn-btn">Search</button>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </form>
+                </div>
+            </section>
+            <div className="container py-5 w-75">
+                <div className="row room-items">
+                    {rooms === null ? (
+                        <Spinner animation="border" />
+                    ) : (
+                        rooms.map(c => (
+                            <div key={c.id} className="col-lg-4 col-12 d-grid justify-content-center pb-5">
+                                <Card style={{ width: '18rem' }}>
+                                    <Card.Img variant="top" src='https://res.cloudinary.com/dc5gyjv8c/image/upload/v1713025498/uyqa8u9seyahpnk0k324.png' />
+                                    <Card.Body>
+                                        <Card.Title>{c.post.title && c.post.title}</Card.Title>
+                                        <Card.Text>
+                                            {c.post.content && c.post.content} <br />
+                                            {c.address && c.address}
+                                        </Card.Text>
+                                        <a href={`/room/${c.id}`} className="cs-btn-detail btn btn-default text-white">Chi tiết</a>
+                                    </Card.Body>
+                                </Card>
+                            </div>
+                        ))
+                    )}
+                </div>
+            </div>
+        </>
+    );
+
+};
+
+export default HomeLandlord;
---
+++ MyHouseWeb_Group1/src/component/register.js
@@ -0,0 +1,115 @@
+import React, { Component, useState, useEffect, useRef } from "react";
+import { useHistory } from 'react-router-dom';
+import { Form, Button } from 'react-bootstrap';
+import Alert from "./common/alert"
+import 'bootstrap/dist/css/bootstrap.min.css';
+import APIs, { API_URL, endpoints } from "../config/APIs";
+
+function Register() {
+
+    const [errorMessage, setErrorMessage] = useState('');
+    const [alertStatus, setAlertStatus] = useState(false);
+    const [alertType, setAlertType] = useState('');
+
+
+    const fields = [{
+        label: "Tên người dùng",
+        type: "text",
+        field: "username"
+    }, {
+        label: "Mật khẩu",
+        type: "password",
+        field: "password"
+    }, {
+        label: "Xác nhận mật khẩu",
+        type: "password",
+        field: "confirm"
+    }, {
+        label: "Email",
+        type: "email",
+        field: "email"
+    }, {
+        label: "Bạn là ?",
+        type: "text",
+        field: "role"
+    }]
+
+    const history = useHistory()
+    const [user, setUser] = useState({});
+    const avatar = useRef();
+
+
+    const change = (e, field) => {
+        setUser(current => {
+            return { ...current, [field]: e.target.value }
+        })
+    }
+
+    const register = async (e) => {
+        e.preventDefault();
+        let form = new FormData();
+        for (let key in user)
+            // if(key === " "){
+            //     setErrorMessage("Vui lòng nhập đầy đủ thông tin")
+            //     setAlertStatus(true)
+            //     setAlertType("error")
+            // } else 
+            if (key !== 'confirm')
+                form.append(key, user[key])
+        if (avatar)
+            form.append('file', avatar.current.files[0])
+        try {
+            let res = await APIs.post(endpoints['register'], form, {
+                headers: {
+                    'Content-Type': 'multipart/form-data'
+                }
+            });
+            if (res.status === 201) {
+                history.push("/login")
+                setErrorMessage("Đăng kí thành công!")
+                setAlertStatus(true)
+                setAlertType("success")
+            } else {
+                setErrorMessage("Đã có lỗi xảy ra")
+                setAlertStatus(true)
+                setAlertType("error")
+            }
+
+
+        } catch (ex) {
+            console.error(ex);
+        }
+    }
+
+    return (
+        <div className="Login">
+            <Form onSubmit={register} className="mt-5">
+                <div className="form-title">
+                    <h2>Đăng Kí</h2>
+                </div>
+
+                {fields.map(f => <Form.Group key={f.field} className="mb-3" controlId={f.field}>
+                    <Form.Label className="float-left">{f.label}</Form.Label>
+                    <Form.Control onChange={e => change(e, f.field)} value={user[f.field]} type={f.type} placeholder={f.label} />
+                </Form.Group>)}
+
+                <Form.Group className="mb-3" controlId="avatar">
+                    <Form.Label className="float-left">Chọn ảnh đại diện</Form.Label>
+                    <Form.Control type="file" accept=".png,.jpg,.jpeg" ref={avatar} />
+                </Form.Group>
+                <Button type="submit" className="mt-3 btn btn-default text-white" variant="primary">
+                    Đăng kí
+                </Button>
+            </Form>
+
+            <Alert
+                status={alertStatus}   // true or false
+                type={alertType}   // success, warning, error, info
+                title={errorMessage}   // title you want to display
+                setIsAlert={setAlertStatus}
+            />
+        </div>
+    );
+}
+
+export default Register;
